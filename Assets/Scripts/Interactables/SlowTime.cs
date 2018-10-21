2242ff556b58039727155831d3ce61dace785fe3
BrunoPasca <42937086+BrunoPasca@users.noreply.github.com>
2018-10-21 16:55:30 -0300

    .

--- Assets/Scripts/Interactables/SlowTime.cs
+++ Assets/Scripts/Interactables/SlowTime.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections;
+﻿
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
 public class SlowTime : ShotInteractable {
 
 
-	[SerializeField] float time;
-	
 	[SerializeField] Transform player;
 
 	Hand[] hands;
---
+++ Assets/Scripts/PlayScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using UnityEngine.SceneManagement;
+public class PlayScene : ShotInteractable
+{
+
+
+    [SerializeField] float time;
+
+    [SerializeField] Transform player;
+
+    Hand[] hands;
+
+    public override void Interact()
+    {
+        base.Interact();
+        SceneManager.LoadScene(1);
+
+
+    }
+
+
+}
