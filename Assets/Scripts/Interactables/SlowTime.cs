d11b1e94348e445d28e79e3945ee6e6f78fb6c46
BrunoPasca <42937086+BrunoPasca@users.noreply.github.com>
2018-10-21 19:22:17 -0300

    Merge branch 'master' of https://github.com/Supernico12/Steam-VR

--- Assets/Scripts/Interactables/SlowTime.cs
+++ Assets/Scripts/Interactables/SlowTime.cs
@@ -1,15 +1,26 @@
+<<<<<<< HEAD
 ﻿
 
 
 using System.Collections;
+=======
+﻿using System.Collections;
+>>>>>>> 036d8778ef64ca5365ce6f6e65adb44f6eef442e
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
 public class SlowTime : ShotInteractable {
 
 
+<<<<<<< HEAD
 	[SerializeField] Transform player;
     public float time;
+=======
+	[SerializeField] float time;
+	
+	[SerializeField] Transform player;
+
+>>>>>>> 036d8778ef64ca5365ce6f6e65adb44f6eef442e
 	Hand[] hands;
 
 	public override void Interact(){
