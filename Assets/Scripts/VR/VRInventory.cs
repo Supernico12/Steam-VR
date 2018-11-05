e9ccba1fe343268f669e41b3b7b6cf6586dd1333
Supernico12 <37745277+Supernico12@users.noreply.github.com>
2018-11-05 12:15:31 -0300

    VR Inventory

---
+++ Assets/Scripts/VR/VRInventory.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using Valve.VR;
+
+public class VRInventory : MonoBehaviour
+{
+
+
+    [SerializeField] SteamVR_Input_Sources thisHand;
+    [SerializeField] SteamVR_Input_Sources otherHand;
+    [SerializeField] GameObject hand1;
+    [SerializeField] GameObject hand2;
+
+    [SerializeField] GameObject[] placeHolders;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SteamVR_Input.__actions_default_in_Inventory.GetStateDown(thisHand))
+        {
+            SpawnPalceHolders(hand1);
+        }
+
+    }
+
+    void SpawnPalceHolders(GameObject hand)
+    {
+        Transform[] position = new Transform[3];
+        for (int i = 0; i < placeHolders.Length; i++)
+        {
+            position[i] = hand.transform.Find("SpawnPosition" + i);
+        }
+
+        for (int i = 0; i < placeHolders.Length; i++)
+        {
+            Instantiate(placeHolders[i], position[i].position, Quaternion.identity);
+        }
+
+
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/VR/WeaponSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class WeaponSpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject weapon;
+    Interactable interactable;
+    Hand.AttachmentFlags weaponFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.TurnOnKinematic) | (Hand.AttachmentFlags.TurnOffGravity) | (Hand.AttachmentFlags.VelocityMovement);
+
+
+
+
+    void HandHoverUpdate(Hand hand)
+    {
+
+        GrabTypes startingGrabTypes = hand.GetGrabStarting();
+
+        if (startingGrabTypes != GrabTypes.None)
+        {
+
+            GameObject instance = Instantiate(weapon, hand.transform.position, Quaternion.identity);
+            Transform offset = instance.transform.Find("Grip").transform;
+            Debug.Log(offset);
+
+            hand.HoverLock(instance.GetComponent<Interactable>());
+            hand.AttachObject(instance, startingGrabTypes, weaponFlags, attachmentOffset: offset);
+
+            Destroy(gameObject, 1);
+
+
+
+
+
+        }
+
+
+    }
+
+
+
+
+
+}
